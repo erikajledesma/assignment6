fe72ff57e21cd9fba0b2acb94131bf2b82117627
sudistix <113730997+scotttlee@users.noreply.github.com>
2023-04-25 17:12:52 -0700

    added functions for lights -momo

--- Intro to Pygame Graphics/major league soccer animation/graphics_v4.py
+++ Intro to Pygame Graphics/major league soccer animation/graphics_v4.py
@@ -202,14 +202,32 @@ while not done:
     pygame.draw.ellipse(screen, GRAY, [150, 195, 20, 10])
 
     #lights1
-    #drawing the bottom row of the left lights
+    # drawing the bottom row of the left lights
     for i in range(90, 190, 20):
+        #draws the line to separate the lights
+        #screen: pygame window
+        #GRAY: color of line
+        #list 1&2: the x and y allignemnt
+        #2: thickness of line
         pygame.draw.line(screen, GRAY, [110, 60], [210, 60], 2)
-        pygame.draw.ellipse(screen, light_color, [i+20, 40, 20, 20])
+        #draws the bottom left row of lights
+        #screen: pygame window
+        #light_color: refers to function to see whether the lights will be yellow or grey
+        #list:(y movement,x movement,length of lightbulb, height of lightbulb)
+        pygame.draw.ellipse(screen, light_color, [i+20, 40, 20, 200])
 
     #drawing the top row of the left light
     for i in range(90, 190, 20):
+        #draws the line to separate the lights
+        #screen: pygame window
+        #GRAY: color of line
+        #list 1&2: the x and y allignemnt
+        #2: thickness of line
         pygame.draw.line(screen, GRAY, [110, 40], [210, 40], 2)
+        #draws the top left row of lights
+        #screen: pygame window
+        #light_color: refers to function to see whether the lights will be yellow or grey
+        #list:(y movement,x movement,length of lightbulb, height of lightbulb)
         pygame.draw.ellipse(screen, light_color, [i + 20, 20, 20, 20])
     pygame.draw.line(screen, GRAY, [110, 20], [210, 20], 2)
 
@@ -221,12 +239,30 @@ while not done:
 
     # draws top right light
     for i in range(570, 670, 20):
+        #draws the line to separate the lights
+        #screen: pygame window
+        #GRAY: color of line
+        #list 1&2: the x and y allignemnt
+        #2: thickness of line
         pygame.draw.line(screen, GRAY, [590, 40], [690, 40], 2)
+        #draws the top right row of lights
+        #screen: pygame window
+        #light_color: refers to function to see whether the lights will be yellow or grey
+        #list:(y movement,x movement,length of lightbulb, height of lightbulb)
         pygame.draw.ellipse(screen, light_color, [i + 20, 20, 20, 20])
 
     # draws bottom right light
     for i in range(570, 670, 20):
+        #draws the line to separate the lights
+        #screen: pygame window
+        #GRAY: color of line
+        #list 1&2: the x and y allignemnt
+        #2: thickness of line
         pygame.draw.line(screen, GRAY, [590, 60], [690, 60], 2)
+        #draws the bottom right row of lights
+        #screen: pygame window
+        #light_color: refers to function to see whether the lights will be yellow or grey
+        #list:(y movement,x movement,length of lightbulb, height of lightbulb)
         pygame.draw.ellipse(screen, light_color, [i + 20, 40, 20, 20])
 
     
@@ -277,6 +313,44 @@ while not done:
     num = 1
     drawLines1(screen, color, list1, list2, num)
 
+
+
+    # pygame.draw.line(screen, WHITE, [325, 140], [341, 200], 1)
+    # pygame.draw.line(screen, WHITE, [330, 140], [344, 200], 1)
+    # pygame.draw.line(screen, WHITE, [335, 140], [347, 200], 1)
+    # pygame.draw.line(screen, WHITE, [340, 140], [350, 200], 1)
+    # pygame.draw.line(screen, WHITE, [345, 140], [353, 200], 1)
+    # pygame.draw.line(screen, WHITE, [350, 140], [356, 200], 1)
+    # pygame.draw.line(screen, WHITE, [355, 140], [359, 200], 1)
+    # pygame.draw.line(screen, WHITE, [360, 140], [362, 200], 1)
+    # pygame.draw.line(screen, WHITE, [364, 140], [365, 200], 1)
+    # pygame.draw.line(screen, WHITE, [368, 140], [369, 200], 1)
+    # pygame.draw.line(screen, WHITE, [372, 140], [373, 200], 1)
+    # pygame.draw.line(screen, WHITE, [376, 140], [377, 200], 1)
+    # pygame.draw.line(screen, WHITE, [380, 140], [380, 200], 1)
+    # pygame.draw.line(screen, WHITE, [384, 140], [384, 200], 1)
+    # pygame.draw.line(screen, WHITE, [388, 140], [388, 200], 1)
+    # pygame.draw.line(screen, WHITE, [392, 140], [392, 200], 1)
+    # pygame.draw.line(screen, WHITE, [396, 140], [396, 200], 1)
+    # pygame.draw.line(screen, WHITE, [400, 140], [400, 200], 1)
+    # pygame.draw.line(screen, WHITE, [404, 140], [404, 200], 1)
+    # pygame.draw.line(screen, WHITE, [408, 140], [408, 200], 1)
+    # pygame.draw.line(screen, WHITE, [412, 140], [412, 200], 1)
+    # pygame.draw.line(screen, WHITE, [416, 140], [416, 200], 1)
+    # pygame.draw.line(screen, WHITE, [420, 140], [420, 200], 1)
+    # pygame.draw.line(screen, WHITE, [424, 140], [423, 200], 1)
+    # pygame.draw.line(screen, WHITE, [428, 140], [427, 200], 1)
+    # pygame.draw.line(screen, WHITE, [432, 140], [431, 200], 1)
+    # pygame.draw.line(screen, WHITE, [436, 140], [435, 200], 1)
+    # pygame.draw.line(screen, WHITE, [440, 140], [438, 200], 1)
+    # pygame.draw.line(screen, WHITE, [445, 140], [441, 200], 1)
+    # pygame.draw.line(screen, WHITE, [450, 140], [444, 200], 1)
+    # pygame.draw.line(screen, WHITE, [455, 140], [447, 200], 1)
+    # pygame.draw.line(screen, WHITE, [460, 140], [450, 200], 1)
+    # pygame.draw.line(screen, WHITE, [465, 140], [453, 200], 1)
+    # pygame.draw.line(screen, WHITE, [470, 140], [456, 200], 1)
+    # pygame.draw.line(screen, WHITE, [475, 140], [459, 200], 1)
+
     def drawLines2(screen, color, list1, list2, num):
         '''draw lines for the second part of the net
     screen: pygame window
@@ -296,45 +370,40 @@ while not done:
     num2 = 1
     drawLines2(screen2, color2, list1_2, list2_2, num2)
 
-    #net part 3
-    def draw_net3(screen, color, num1, num2, width):
-        ''' draws lines for the third part of the net
-            screen: python display screen
-            color: sets color of lines
-            num1: x-value of ending vector for line
-            num2: y-value of ending vector for line
-            width: thickness of line '''
-        for n in range(9):
-            pygame.draw.line(screen, color, [480,140], [num1, num2], width)
-            num1 = num1 - 2
-            num2 = num2 - 2
+    # pygame.draw.line(screen, WHITE, [320, 140], [324, 216], 1)
+    # pygame.draw.line(screen, WHITE, [320, 140], [326, 214], 1)
+    # pygame.draw.line(screen, WHITE, [320, 140], [328, 212], 1)
+    # pygame.draw.line(screen, WHITE, [320, 140], [330, 210], 1)
+    # pygame.draw.line(screen, WHITE, [320, 140], [332, 208], 1)
+    # pygame.draw.line(screen, WHITE, [320, 140], [334, 206], 1)
+    # pygame.draw.line(screen, WHITE, [320, 140], [336, 204], 1)
+    # pygame.draw.line(screen, WHITE, [320, 140], [338, 202], 1)
 
-    draw_net3(screen, WHITE, 476, 216, 1)
+    #net part 3
+    pygame.draw.line(screen, WHITE, [480, 140], [476, 216], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [474, 214], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [472, 212], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [470, 210], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [468, 208], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [466, 206], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [464, 204], 1)
+    pygame.draw.line(screen, WHITE, [480, 140], [462, 202], 1)
 
     #net part 4
-    def draw_net4(screen, color, num1, num2, width):
-        ''' draws lines for the fourth part of the net
-            screen: python display screen
-            color: sets color of lines
-            num1: y-value of starting vector for line
-            num2: x-value of ending vector for line
-            width: thickness of line '''
-        for n in range(10):
-            pygame.draw.line(screen, color, [324, num1], [476, num2], width)
-            num1 = num1 + 4
-            num2 = num2 + 4
-
-        pygame.draw.line(screen, color, [335, 180], [470, 180], 1)
-
-        num1 = 184
-        num2 = 184
-
-        for n in range(5):
-            pygame.draw.line(screen, color, [335, num1], [465, num2], width)
-            num1 = num1 + 4
-            num2 = num2 + 4
-    
-    draw_net4(screen, WHITE, 144, 144, 1)
+    pygame.draw.line(screen, WHITE, [324, 144], [476, 144], 1)
+    pygame.draw.line(screen, WHITE, [324, 148], [476, 148], 1)
+    pygame.draw.line(screen, WHITE, [324, 152], [476, 152], 1)
+    pygame.draw.line(screen, WHITE, [324, 156], [476, 156], 1)
+    pygame.draw.line(screen, WHITE, [324, 160], [476, 160], 1)
+    pygame.draw.line(screen, WHITE, [324, 164], [476, 164], 1)
+    pygame.draw.line(screen, WHITE, [324, 168], [476, 168], 1)
+    pygame.draw.line(screen, WHITE, [324, 172], [476, 172], 1)
+    pygame.draw.line(screen, WHITE, [324, 176], [476, 176], 1)
+    pygame.draw.line(screen, WHITE, [335, 180], [470, 180], 1)
+    pygame.draw.line(screen, WHITE, [335, 184], [465, 184], 1)
+    pygame.draw.line(screen, WHITE, [335, 188], [465, 188], 1)
+    pygame.draw.line(screen, WHITE, [335, 192], [465, 192], 1)
+    pygame.draw.line(screen, WHITE, [335, 196], [465, 196], 1)
 
     #stands right
     pygame.draw.polygon(screen, RED, [[680, 220], [800, 340], [800, 290], [680, 180]])
